a241d5b8a8c90742b825d2c97e57b09cfbe70fad
Viet Anh Nguyen <vietanh@vietanhdev.com>
2017-03-26 15:19:00 +0700

    make it better

---
+++ assets/js/hihi_chatbot.js
@@ -0,0 +1,91 @@
+$(document).ready(function() {
+  // Append chat into page
+  var chatHtml = '<div class="chatbox-wrapper">';
+  chatHtml += '<div class="title">';
+  chatHtml += '<i class="fa fa-user-o" aria-hidden="true"></i>';
+  chatHtml += '<span>Jarvis</span>';
+  chatHtml += '<span class="toggle-chat" role="button">Open</span>';
+  chatHtml += '</div>';
+  chatHtml += '<div class="messages"></div>';
+  chatHtml += '<div class="sendbox">';
+  chatHtml += '<input type="text" class="newmessage" placeholder="type and enter">';
+  chatHtml += '<button class="send"><i class="fa fa-paper-plane" aria-hidden="true"></i></button>';
+  chatHtml += '</div>';
+  chatHtml += '</div>';
+  $('body').append(chatHtml);
+
+
+  // Say Hello
+  // addMessage(1, 'Hello! I\'m the chatbot of this page.');
+  // addMessage(1, 'If you want to search something or ask about this page, feel free to ask me. I can try to answer your questions or send them to my boss - Viet Anh.');
+  addMessage(1, 'Chào bạn! Tôi là chatbot của trang này.');
+  addMessage(1, 'Bạn có thể đặt câu hỏi hoặc trò chuyện tại đây. Chatbot đang được hoàn thành nên có thể chưa đầy đủ chức năng hoặc gặp nhiều lỗi nhỏ.');
+
+  // Scroll to last message
+  $('.messages').animate({scrollTop: $('.messages').prop("scrollHeight")}, 500);
+
+  // Bind keys
+  $(document).keypress(function(e){
+      if (e.which == 13 && $(".chatbox-wrapper .newmessage").is(":focus")){
+          $(".chatbox-wrapper .send").click();
+          return false;
+      }
+  });
+
+  // Click send button
+  var currUser = 1;
+$('.chatbox-wrapper .send').click(function() {
+    var message = $('.sendbox .newmessage').val();
+    addMessage(2, message);
+
+    // Scroll to last message
+    $('.messages').animate({scrollTop: $('.messages').prop("scrollHeight")}, 500);
+    $('.sendbox .newmessage').val('');
+
+    $.ajax({
+        url: "https://vbot-api.herokuapp.com/chat",
+        type: "POST",
+        crossDomain: true,
+        data: {"message": message},
+        dataType: "json",
+        success: function (data) {
+            console.log(data);
+            if (data.reply != '') {
+              addMessage(1, data.reply);
+              // Scroll to last message
+              $('.messages').animate({scrollTop: $('.messages').prop("scrollHeight")}, 500);
+            }
+        },
+        error: function (xhr, status) {
+            alert("error");
+        }
+    });
+  });
+
+  // Click chat title bar
+  $('.chatbox-wrapper .title').click(function() {
+    if($('.chatbox-wrapper .toggle-chat').html() == 'Close') {
+      $('.chatbox-wrapper .toggle-chat').html('Open');
+    } else {
+      $('.chatbox-wrapper .toggle-chat').html('Close');
+    }
+    $('.chatbox-wrapper .messages').slideToggle(200);
+    $('.chatbox-wrapper .sendbox').slideToggle(200);
+  });
+
+  function addMessage(userId, message) { 
+    if (userId == 1) {
+      $('.messages').append('<div class="message u1"><span class="content">'+message+'</span><span class="ff"></span></div>');
+    } else {
+      $('.messages').append('<div class="message u2"><span class="ff"></span><span class="content">'+message+'</span></div>');
+    }
+  }
+
+  window.addEventListener("load",function() {
+      setTimeout(function(){
+          // This hides the address bar:
+          window.scrollTo(0, 1);
+      }, 0);
+  });
+
+});
