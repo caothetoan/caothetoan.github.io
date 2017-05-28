8789b31dc0d8bceccba2c7a042825e35b65f12e2
Viet Anh Nguyen <vietanh@vietanhdev.com>
2017-05-28 23:21:58 +0700

    add search site

--- search/search.js
+++ search/search.js
@@ -71,8 +71,8 @@ $(document).ready(function() {
           var item = loaded_data[result.ref];
 
           // Build a snippet of HTML for this result
-          var appendString = '<li><h3><a href="' + item.url + '">' + item.title + '</a>';
-          appendString += '<p>' + item.content.slice(1, 60) + '...</p></h3></li>';
+          var appendString = '<li><h3><a href="' + item.url + '">' + item.title + '</a></h3>';
+          appendString += '<p>' + item.content.slice(1, 200) + '...</p></li>';
 
           // Add the snippet to the collection of results.
           $search_results.append(appendString);
