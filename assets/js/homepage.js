89dbb6d23456bec0ba1916e28f758e99efce65d5
Cao Thế Toàn <thetoan.cao@labo.io>
2017-06-14 16:02:30 +0700

    commit

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$('*[verlang="vi"]').hide();
 	function helloWorld(){
 		$("#typed").typed({
-			strings: ["Hi!",{{site.about.english}}, "I'm a developer.", "I'd like robotic.",   "Nice to meet you!"],
+			strings: ["Hi!","{{site.about.english}}", "I'm a developer.", "I'd like robotic.",   "Nice to meet you!"],
 			cursorChar: " &#9749;",
 			typeSpeed: 45,
 			callback: function(){
